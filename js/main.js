a2ec02a320afd71ecfcb855b2b69eb0f420c7c08
Johany Navarro <johany87@gmail.com>
2016-10-13 15:56:48 -0500

    Second commit erros fixed

--- js/main.js
+++ js/main.js
@@ -1,70 +1,78 @@
 //VARIABLES GLOBALES
-	var turno = 1; //VARIABLE PARA ALTERNAR ENTRE X Y O
-	var gana = false; //VARIABLE PARA ENVIAR A LA FUNCTION GANADOR
-	var contador = 1; //VARIABLE PARA CONTAR LOS TURNOS
+	var turn = 1; //VARIABLE PARA ALTERNAR ENTRE X Y O
+	var win = false; //VARIABLE PARA ENVIAR A LA FUNCTION GANADOR
+	var count = 1; //VARIABLE PARA CONTAR LOS turnS
 	var player1; // ALMACENA EL NOMBRE DEL PRIMER JUGADOR
 	var player2; // ALMACENA EL NOMBRE DEL SEGUNDO JUGADOR
-	var signals; // ELEMENTO HTML PARA MOSTRAR LOS TURNOS
+	var signals; // ELEMENTO HTML PARA MOSTRAR LOS turnS
+	var pos; //POSICION DONDE SE DA CLICK
+
 //FUNCION PARA ESTABLECER GANADOR
-	function ganador(gana) {
-		if(gana === true){
-			if ((tablero[0].textContent=="X"&& tablero[1].textContent=="X"&& tablero[2].textContent=="X") ||
-					 (tablero[3].textContent=="X"&& tablero[4].textContent=="X"&& tablero[5].textContent=="X") ||
-					 (tablero[6].textContent=="X"&& tablero[7].textContent=="X"&& tablero[8].textContent=="X") ||
-					 (tablero[0].textContent=="X"&& tablero[3].textContent=="X"&& tablero[6].textContent=="X") ||
-				     (tablero[1].textContent=="X"&& tablero[4].textContent=="X"&& tablero[7].textContent=="X") ||
-				     (tablero[2].textContent=="X"&& tablero[5].textContent=="X"&& tablero[8].textContent=="X") ||
-				     (tablero[0].textContent=="X"&& tablero[4].textContent=="X"&& tablero[8].textContent=="X") ||
-				     (tablero[2].textContent=="X"&& tablero[4].textContent=="X"&& tablero[6].textContent=="X")) {
-				   alert("Ganador: "+player1);
-			}	else if((tablero[0].textContent=="O" && tablero[1].textContent=="O" && tablero[2].textContent=="O") ||
-					 (tablero[3].textContent=="O" && tablero[4].textContent=="O" && tablero[5].textContent=="O") ||
-					 (tablero[6].textContent=="O" && tablero[7].textContent=="O" && tablero[8].textContent=="O") ||
-					 (tablero[0].textContent=="O" && tablero[3].textContent=="O" && tablero[6].textContent=="O") ||
-				     (tablero[1].textContent=="O" && tablero[4].textContent=="O" && tablero[7].textContent=="O") ||
-				     (tablero[2].textContent=="O" && tablero[5].textContent=="O" && tablero[8].textContent=="O") ||
-				     (tablero[0].textContent=="O" && tablero[4].textContent=="O" && tablero[8].textContent=="O") ||
-				     (tablero[2].textContent=="O" && tablero[4].textContent=="O" && tablero[6].textContent=="O")) {
-					 alert("Ganador: "+player2);
-			}
-		}else{
-		}
+	function winner() {
+				console.log("valido win = true");
+				if ((board[0].textContent=="X"&& board[1].textContent=="X"&& board[2].textContent=="X") ||
+						 (board[3].textContent=="X"&& board[4].textContent=="X"&& board[5].textContent=="X") ||
+						 (board[6].textContent=="X"&& board[7].textContent=="X"&& board[8].textContent=="X") ||
+						 (board[0].textContent=="X"&& board[3].textContent=="X"&& board[6].textContent=="X") ||
+					   (board[1].textContent=="X"&& board[4].textContent=="X"&& board[7].textContent=="X") ||
+					   (board[2].textContent=="X"&& board[5].textContent=="X"&& board[8].textContent=="X") ||
+					   (board[0].textContent=="X"&& board[4].textContent=="X"&& board[8].textContent=="X") ||
+					   (board[2].textContent=="X"&& board[4].textContent=="X"&& board[6].textContent=="X")) {
+						 win = true;
+						 alert("Ganador: "+player1);
+				}	else if((board[0].textContent=="O" && board[1].textContent=="O" && board[2].textContent=="O") ||
+						 (board[3].textContent=="O" && board[4].textContent=="O" && board[5].textContent=="O") ||
+						 (board[6].textContent=="O" && board[7].textContent=="O" && board[8].textContent=="O") ||
+						 (board[0].textContent=="O" && board[3].textContent=="O" && board[6].textContent=="O") ||
+					   (board[1].textContent=="O" && board[4].textContent=="O" && board[7].textContent=="O") ||
+					   (board[2].textContent=="O" && board[5].textContent=="O" && board[8].textContent=="O") ||
+					   (board[0].textContent=="O" && board[4].textContent=="O" && board[8].textContent=="O") ||
+					   (board[2].textContent=="O" && board[4].textContent=="O" && board[6].textContent=="O")) {
+						 win = true;
+						 alert("Ganador: "+player2);
+				}
 	}
-//FUNCION DIBUJAR EN LAS celdas
-	function dibujar(evento){
-		posicion = evento.target.id.charAt(1);
-		if(contador > 4){
-			gana=true;
-			ganador(gana);
-		}
-		if (turno % 2 !== 0) {
-			if (tablero[posicion] == "X" || tablero[posicion] == "O"){
+
+//FUNCION draw EN LAS celdas
+	function draw(evento){
+		pos = evento.target.id.charAt(1);
+		if (turn % 2 !== 0) {
+			if (board[pos] == "X" || board[pos] == "O"){
 				/*NO HACE NADA PORQUE LA CELDA ESTA OCUPADA*/
 			}else {
-				tablero[posicion].textContent = "X";
-				tablero[posicion].style.background="#DB5734";
-				document.getElementById("h2signals").textContent = player2;
-				turno++;
-				contador++;
+				console.log(turn, count);
+				board[pos].textContent = "X";
+				document.getElementById("h2signals").textContent = "Turno de: " +player2;
+				turn++;
+				count++;
+
 			}
 		}else {
-			if (tablero[posicion] == "X" || tablero[posicion] == "O"){
+			if (board[pos] == "X" || board[pos] == "O"){
 				/*NO HACE NADA PORQUE LA CELDA ESTA OCUPADA*/
 			}else {
-				tablero[posicion].textContent = "O";
-				tablero[posicion].style.background="#DB5734";
-				document.getElementById("h2signals").textContent = player1;
-				turno++;
-				contador++;
+				console.log(turn, count);
+				board[pos].textContent = "O";
+				document.getElementById("h2signals").textContent = "Turno de: " +player1;
+				turn++;
+				count++;
 			}
 		}
-	}//DIBUJAR
+		if(count >= 5 && count <9){
+			winner();
+		}
+		if (count >= 10) {
+			alert("EMPATE!!! \nPresiona el boton Voler a jugar!!");
+		}
+	}//draw
+
 //FUNCION PARA RECARGAR EL JUEGO
 	function play_again(){
 		window.location.reload();
 	}
+
 // VARIABLE PARA ALMACENAR LA TABLA
-	var tablero;
+	var board;
 
 document.addEventListener("DOMContentLoaded", function(){
 		//CAPTURAMOS LOS NOMBRES DE LOS JUGADORES
@@ -76,14 +84,14 @@ document.addEventListener("DOMContentLoaded", function(){
 		//MOSTRAMOS LOS NOMBRES EN EL ELEMENTO HTML
 			namep1.textContent = player1;
 			namep2.textContent = player2;
-		//MOSTRAMOS EL PRIMER TURNO
+		//MOSTRAMOS EL PRIMER turn
 			signals = document.querySelector("#h2signals");
-			signals.textContent = player1;
-		//CREAMOS EL ARRAY PARA ALMACENAR LAS POSICIONES
-			tablero = document.querySelectorAll(".square");
+			signals.textContent = "turn de: " +player1;
+		//CREAMOS EL ARRAY PARA ALMACENAR LAS posES
+			board = document.querySelectorAll(".square");
 		//AGREGAMOS EL ESCUCHA A LAS CELDAS DE LA TABLA
-			for (var i=0; i<tablero.length; i++){
-				tablero[i].addEventListener("click", dibujar);
+			for (var i=0; i<board.length; i++){
+				board[i].addEventListener("click", draw);
 			}
 		//CAPTURAMOS EL BOTON Y AGREGAMOS EL ESCUCHA PARA PARA INICIAR DE NUEVO EL JUEGO
 			var restart = document.querySelector("#again");
